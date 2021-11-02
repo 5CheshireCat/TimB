c87c3f3931855733f2e72a4d1a4f7664d5483aa8
Basov Timur <timurbasov123@gmail.com>
2021-11-02 14:03:07 +0200

    add homework3

---
+++ src/com/java/homework3/Task1.java
@@ -0,0 +1,9 @@
+package com.java.homework3;
+
+import java.util.Random;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println(new Random(). nextDouble());
+    }
+}
