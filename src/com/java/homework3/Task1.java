61dde2290d9b9d69ef7bef33298866142c2c0012
Basov Timur <timurbasov123@gmail.com>
2021-11-06 14:20:34 +0200

    remove

--- src/com/java/homework3/Task1.java
+++
@@ -1,9 +0,0 @@
-package com.java.homework3;
-
-import java.util.Random;
-
-public class Task1 {
-    public static void main(String[] args) {
-        System.out.println(new Random(). nextDouble());
-    }
-}
