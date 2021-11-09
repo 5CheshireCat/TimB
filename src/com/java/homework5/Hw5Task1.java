89e720ac73e38ea1527be1ee113bf2208aeacea6
Basov Timur <timurbasov123@gmail.com>
2021-11-09 11:15:16 +0200

    Add Homework5

--- src/com/java/homework5/Hw5Task1.java
+++ src/com/java/homework5/Hw5Task1.java
@@ -9,14 +9,15 @@ public class Hw5Task1 {
         for (int i = 0; i < massive.length; i++) {
             massive[i] = (int) (Math.random() * 10) + 1;
         }
+        
         System.out.println(Arrays.toString(massive));
+
         for (int i = 0; i < massive.length; i++) {
             if (i % 2 == 1) {
                 massive[i] = 0;
             }
-            System.out.println(Arrays.toString(massive));
-
         }
+        System.out.println(Arrays.toString(massive));
 
     }
 }
